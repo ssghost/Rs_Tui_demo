7d71de047a674074650bc524517d7aaaecd1c717
ssghost <ssprof0@gmail.com>
2023-09-04 19:41:30 +0800

    add term api

--- src/tui.rs
+++ src/tui.rs
@@ -9,10 +9,29 @@ use crossterm::{
 use miette::Result;
 use ratatui::{prelude::*, widgets::*};
 use std::{
-    io::{self, Stdut},
+    io::{self, Stdout},
     sync::{Arc, Mutex},
     time::Duration,
 };
 use crate::AppState;
 
+pub fn setup_terminal() -> Result<Terminal<CrosstermBackend<Stdout>>, io::Error> {
+    let mut stdout = io::stdout();
+    enable_raw_mode()?;
+    execute!(stdout, EnterAlternateScreen)?;
+    Ok(
+        Terminal::new(CrosstermBackend::new(stdout,))?
+    )
+}
+
+pub fn restore_terminal(
+    terminal: &mut Terminal<CrosstermBackend<Stdout>>,
+) -> Result<(), io::Error> {
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+    )?;
+    Ok(terminal.show_cursor()?)
+}
 
